bb37454e6285c28987c676bb38844be696ce2f09
Varun Chaudhary <varunc105@gmail.com>
2025-04-24 19:00:59 +0530

    feat: basic navbar with fragments

---
+++ app/src/main/java/com/example/lokaljobs/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lokaljobs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNav = findViewById(R.id.bottom_nav)
+        NavigationUI.setupWithNavController(bottomNav, navController)
+    }
+}
